4e48e518e750348b8001661fcfcbedf43e323f05
LeePu <leepupu@gmail.com>
2016-08-05 22:50:38 -0700

    add install script to link config file inside dotfiles folder

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+cur=`pwd`
+home="$HOME"
+function backup() {
+    local profile=$1
+    mv "$home/$profile" "$home/$profile.bak"
+}
+
+function link() {
+    local profile=$1
+    ln -s "$cur/$profile" "$home/$profile"
+}
+
+function proc() {
+    backup $1
+    link $1
+}
+
+proc ".zshrc"
+proc ".vimrc"
+proc ".tmux.conf"
+proc ".gdbinit"
